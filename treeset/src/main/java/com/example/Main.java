b7d236c4e0c159168be9bc070dd074e71c0257b4
RicardoArevaloB <160280340+RicardoArevaloB@users.noreply.github.com>
2025-02-25 10:19:27 -0500

    Programa Terminado

--- treeset/src/main/java/com/example/Main.java
+++ treeset/src/main/java/com/example/Main.java
@@ -6,37 +6,38 @@ import java.util.TreeSet;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        TreeSet<String> palabras = new TreeSet<>();
+        try (Scanner sc = new Scanner(System.in)) {
+            TreeSet<String> palabras = new TreeSet<>();
 
-        System.out.println("Ingrese varias palabras para terminar el programa escribe (fin): ");
+            System.out.println("Ingrese varias palabras para terminar el programa escribe (fin): ");
 
-        // 1. Pedir al usuario que ingrese varias palabras
-        while (true) {
-            System.out.print("Palabra: ");
-            String palabra = scanner.nextLine();
+            while (true) {
+                System.out.print("Palabra: ");
+                String palabra = sc.nextLine();
 
-            if (palabra.equalsIgnoreCase("fin")) {
-                break;
+                if (palabra.equalsIgnoreCase("fin")) {
+                    break;
+                }
+
+                palabras.add(palabra);
             }
 
-            palabras.add(palabra);
+            System.out.println("\nLista de palabras ordenadas alfabéticamente:");
+            for (String palabra : palabras) {
+                System.out.println("- " + palabra);
+            }
+
+            System.out.print("\nIngrese una palabra para buscar en la lista: ");
+            String palabraBuscar = sc.nextLine();
+
+            if (palabras.contains(palabraBuscar)) {
+                System.out.println("La palabra '" + palabraBuscar + "' SÍ está en la lista.");
+            } else {
+                System.out.println("La palabra '" + palabraBuscar + "' NO está en la lista.");
+            }
+
+            sc.close();
         }
 
-        System.out.println("\nLista de palabras ordenadas alfabéticamente:");
-        for (String palabra : palabras) {
-            System.out.println("- " + palabra);
-        }
-
-        System.out.print("\nIngrese una palabra para buscar en la lista: ");
-        String palabraBuscar = scanner.nextLine();
-
-        if (palabras.contains(palabraBuscar)) {
-            System.out.println("La palabra '" + palabraBuscar + "' SÍ está en la lista.");
-        } else {
-            System.out.println("La palabra '" + palabraBuscar + "' NO está en la lista.");
-        }
-
-        scanner.close();
     }
 }
